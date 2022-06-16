ecdcdf769765f0aa43f62f470c814a2c7bcabd38
SusanBird <susanbird391@gmail.com>
2022-06-15 18:13:28 -0700

    all tests passing

--- exercise_13/exercise_13.sql
+++ exercise_13/exercise_13.sql
@@ -1,3 +1,13 @@
 -- the average rental amount by country
 -- include the country name and avg amount
 -- use AVG to average the amount
+
+SELECT country.country, AVG(payment.amount) AS avg
+FROM country
+INNER JOIN city ON city.country_id = country.country_id
+INNER JOIN address ON city.city_id = address.city_id
+INNER JOIN customer ON address.address_id = customer.address_id
+INNER JOIN payment ON customer.customer_id = payment.customer_id
+GROUP BY country.country_id
+ORDER BY avg DESC
+LIMIT 10
--- exercise_14/exercise_14.sql
+++ exercise_14/exercise_14.sql
@@ -1,2 +1,8 @@
 -- the average film length by category
 -- include the category name and avg length
+
+SELECT category.name, AVG(film.length) AS avg
+FROM film
+INNER JOIN film_category ON film.film_id = film_category.film_id
+INNER JOIN category ON film_category.category_id = category.category_id
+GROUP BY category.name
